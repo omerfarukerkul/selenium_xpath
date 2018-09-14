03d8443a939a1acc234959eceadc0823b73d523c
Ömer Faruk Erkul <omerfaruk@dilisim.com>
2018-09-14 17:30:02 +0300

    added ClickOn

--- src/main/java/RunJquery.java
+++ src/main/java/RunJquery.java
@@ -1,17 +1,17 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 public class RunJquery {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver webDriver = new ChromeDriver();
+    public static void main(String[] args) throws IOException {
+        ChromeDriver webDriver = new ChromeDriver();
 
         webDriver.manage().window().maximize();
         webDriver.manage().deleteAllCookies();
@@ -25,12 +25,20 @@ public class RunJquery {
                 .stream()
                 .map(WebElement::getText)
                 .filter(Objects::nonNull)
-                .filter(str -> str.length()>0)
+                .filter(str -> str.length() > 0)
                 .forEach(System.out::println);
 
+        FileUtils.copyFile(webDriver.getScreenshotAs(OutputType.FILE), new File(System.getProperty("user.dir"), "deneme.png"));
+
         //Dynamic path
-        webDriver.findElement(By.xpath("//input[contains(@button-renderer,'')"));
+//        webDriver.findElement(By.xpath("//input[contains(@button-renderer,'')");
+//        https://www.youtube.com/watch?v=rIFnN15nSJU&list=PLFGoYjJG_fqo4oVsa6l_V-_7-tzBnlulT&index=9
 
         webDriver.quit();
+    }
+
+    public static void clickOn(WebDriver webDriver, WebElement locator, int timeout) {
+        new WebDriverWait(webDriver, timeout).ignoring(StaleElementReferenceException.class).until(ExpectedConditions.elementToBeClickable(locator));
+        locator.click();
     }
 }
